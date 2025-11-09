d1fd075a30a59e4f2f92210b2872b894791b157c
2021wa15843 <2021wa15843@wilp.bits-pilani.ac.in>
2025-11-09 15:55:24 +0530

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -54,4 +54,6 @@ def export_csv():
     return send_file("static/charts/filtered_students.csv", as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
